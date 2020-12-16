64e60278ad563183c981a6162da5d3afd7279e5d
Anand Pandey <anandp332@gmail.com>
2020-12-16 20:45:26 +0530

    adding buttons

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,5 +1,14 @@
 import React from "react";
 class CartItem extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            price:'Rs 999',
+            title:'Mobile Phone',
+            Qty:'Qty: 1',
+            img: ""
+        }
+    }
     render(){
         return(
             <div className="cart-item">
@@ -8,11 +17,14 @@ class CartItem extends React.Component{
 
                 </div>
                 <div className="right-block">
-                    <div style={ {fontSize:25}}>Phone </div>
-                    <div style={ {fontSize:25,color:'#777'}}>Rs: 999</div>
-                    <div style={ {fontSize:25, color:'#777'}}>Qty: 1</div>
+                    <div style={ {fontSize:25}}>{this.state.title} </div>
+                    <div style={ {fontSize:25,color:'#777'}}>{this.state.price}</div>
+                    <div style={ {fontSize:25, color:'#777'}}>{this.state.Qty}</div>
                     <div className="cart-item-actions">
                             {/* Buttons    */}
+                            <img alt="increase" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/1828/1828926.svg"/>
+                            <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/992/992683.svg"/>
+                            <img alt="delete" className="action-icons" src="https://www.flaticon.com/premium-icon/icons/svg/2874/2874821.svg"/>
                     </div>
 
                 </div>
@@ -24,8 +36,8 @@ class CartItem extends React.Component{
 }
 const styles={
     image: {
-        height: 110,
-        width: 110,
+        height: 160,
+        width: 140,
         borderRadius: 4, 
         background:'#ccc'
     }
